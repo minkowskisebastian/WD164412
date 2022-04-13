286e05df873aa20b25f8b51ddd4a42cbee2e0ac4
seb <91944596+minkowskisebastian@users.noreply.github.com>
2022-04-13 14:50:37 +0200

    poprawienie numeracji, dodanie zadań lab_06

--- lab_06/main.py
+++ lab_06/main.py
@@ -1,53 +1,100 @@
 import numpy as np
+import math
+import random as r
+
+# lab_06
 
 # zadanie 1
 
-a = np.arange(4, 81, 4)
-print(a)
+a = np.arange(3)
+b = np.arange(4, 3 * 4 + 1, 4)
+print(a * b)
 
 
 # zadanie 2
 
-b = np.arange(1.0, 2.0, 0.12, dtype='float')
-c = b.astype('int32')
-print(c)
-print(c.shape)
-print(c.dtype)
-print(c.ndim)
+a2 = np.arange(5, 5*9+1, 5).reshape((3, 3))
+b2 = np.arange(16).reshape((4, 4))
+
+print(np.amin(a2, axis=0))
+print(np.amin(a2, axis=1))
+print(np.amin(b2, axis=0))
+print(np.amin(b2, axis=1))
 
 
 # zadanie 3
 
-def pot(n: int):
-    m = np.identity(n, int)
-    for i in range(n):
-        for j in range(n):
-            m[i, j] = 2 ** (i + j)
-    return m
-
-
-print(pot(5))
+print(a.dot(b.T))
 
 
 # zadanie 4
 
-def generuj(n, m):
-    a = np.arange(m)
-    for i in range(m):
-        a[i] = n ** (i + 1)
-    return a
-
-
-print(generuj(3, 5))
+a4 = np.array((1, 2, 3))
+a5 = np.array((math.e, 2.50, 21.37))
+print(a4 * a5)
 
 
 # zadanie 5
 
-def odwr(n: int):
-    v = [n - i for i in range(n)]
-    return np.diag([i for i in v],2)
+c = np.zeros((2, 3))
 
+for i in range(2):
+    for j in range(3):
+        c[i][j] = r.randint(0, 100)
 
-print(odwr(5))
+a = np.sin(c)
+print(a)
+
 
 # zadanie 6
+
+b = np.cos(c)
+print(b)
+
+
+# zadanie 7
+
+print(a + b) # (???)
+
+
+# zadanie 8
+
+e = np.arange(3, 3 * 9 + 1, 3).reshape((3, 3))
+
+for i in e:
+    print(i)
+
+
+# zadanie 9
+
+for i in e.flat:
+    print(i)
+
+
+# zadanie 10
+
+w = np.arange(81).reshape((9, 9))
+print(w)
+
+w = np.reshape(w, newshape=(27,3))
+print(w)
+
+# wielkość macierzy musi się zgadzać z ilością elementów dlatego w przykładzie 9 * 9 = 81 = 27 * 3
+
+
+# zadanie 11
+
+o = np.arange(12).reshape((3, 4))
+o2 = np.reshape(o, newshape=(4, 3))
+o3 = np.reshape(o, newshape=(2, 6))
+
+for i in o.flat:
+    print(i)
+
+for i in o2.flat:
+    print(i)
+
+for i in o3.flat:
+    print(i)
+
+# wyniki są takie same
