09471b088698d3bb76a5d6fb48d7a137971a9baa
seb <91944596+minkowskisebastian@users.noreply.github.com>
2022-04-27 15:21:45 +0200

    wykonane zadania lab_07
    raczej

--- lab_07/main.py
+++ lab_07/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # ZADANIE 1
@@ -5,9 +6,39 @@ imiona = pd.ExcelFile('datasets/imiona.xlsx')
 df = pd.read_excel(imiona, header=0)
 
 # ZADANIE 2
+
+# a)
 print(df[df.Liczba > 1000])
+# b)
 print(df[df.Imie == 'SEBASTIAN'])
+# c)
 print(df.agg({'Liczba': ['sum']}))
+# d)
 print(df.where((df.Rok >= 2000) & (df.Rok <= 2005)).groupby(by='Rok').agg({'Liczba': ['sum']}))
+# e)
 print(df.groupby(by='Plec').agg({'Liczba': ['sum']}))
-print(df[df.Plec == 'K'].groupby(by='Rok').agg({'Liczba': ['max']}))
+# f)
+
+# g)
+
+
+# ZADANIE 3
+zam = pd.read_csv('datasets/zamowienia.csv', header=0, sep=';', decimal='.')
+print(zam)
+
+# a)
+print(zam['Sprzedawca'].unique())
+# b)
+print(zam['Utarg'].sort_values(ascending=False).head(5))
+# c)
+print(zam[['Sprzedawca', 'idZamowienia']].groupby(['Sprzedawca']).agg({'idZamowienia': ['count']}))
+# d)
+print(zam[['Kraj', 'idZamowienia']].groupby(['Kraj']).agg({'idZamowienia': ['count']}))
+# e)
+print(zam[(zam['Kraj'] == 'Polska') & (pd.to_datetime(zam['Data zamowienia']).dt.year == 2005)].agg(
+    {'idZamowienia': ['count']}))
+# f)
+print(zam[pd.to_datetime(zam['Data zamowienia']).dt.year == 2004].agg({'Utarg': ['mean']}))
+# g)
+zam[pd.to_datetime(zam['Data zamowienia']).dt.year == 2004].to_csv('zamówienia_2004.csv', index=False)
+zam[pd.to_datetime(zam['Data zamowienia']).dt.year == 2005].to_csv('zamówienia_2005.csv', index=False)
